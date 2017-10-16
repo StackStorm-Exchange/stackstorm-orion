a4857f5e32baa9d0e4ba157f527264d2946b28a8
Bradley Bishop <bradley.bishop@encore.tech>
2017-10-16 12:52:01 -0400

    Fixed linting errors

--- tests/test_action_get_agent_id.py
+++ tests/test_action_get_agent_id.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-#import mock
+# import mock
 
 from orion_base_action_test_case import OrionBaseActionTestCase
 
